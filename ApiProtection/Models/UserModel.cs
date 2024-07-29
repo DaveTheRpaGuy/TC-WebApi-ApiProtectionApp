b9d2530a2c2927c214a92d93c55bf74726df6fd1
DaveTheRpaGuy <dave.morris.atl@gmail.com>
2024-07-28 21:46:00 -0400

    Add project files.

---
+++ ApiProtection/Models/UserModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiProtection.Models;
+
+public class UserModel
+{
+    // Note that this is for demo purposes so this might be too much stuff to put in one model
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(10)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(10)]
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    
+    public string EmailAddress { get; set; }
+
+    [Phone]
+    public string PhoneNumber { get; set; }
+
+    [Url]
+    public string Homepage { get; set; }
+
+    [Range(0,5)]
+    public int NumberOfVehicles { get; set; }
+}
